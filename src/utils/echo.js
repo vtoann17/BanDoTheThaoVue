f57e599f84fbfc5165175935d2d144c903dd3652
nguyenquanghuyhoang <hoangnqhpk04092@gmail.com>
2026-04-25 07:37:29 +0700

    Updatefixbug

--- src/utils/echo.js
+++ src/utils/echo.js
@@ -4,11 +4,11 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 export const echo = new Echo({
-    broadcaster: 'reverb', // Hoặc 'pusher' nếu bạn dùng Pusher.com
+    broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wsHost: import.meta.env.VITE_REVERB_HOST || '127.0.0.1', // Đảm bảo dùng IP
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    forceTLS: false, // Tắt TLS vì bạn đang chạy http:// local
     enabledTransports: ['ws', 'wss'],
 });
